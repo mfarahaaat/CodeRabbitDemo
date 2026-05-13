dd14c9ad813b4ab840a0e030497e49add533c62a
Farahat <m.farahat@maxab.io>
2026-05-13 17:36:29 +0300

    add TaskInput component and update MainActivity for task management

--- app/src/main/java/com/coderabbit/demo/MainActivity.kt
+++ app/src/main/java/com/coderabbit/demo/MainActivity.kt
@@ -7,13 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.coderabbit.demo.model.TaskCategory
-import com.coderabbit.demo.ui.components.TaskInput
+import com.coderabbit.demo.ui.screens.TaskCreationScreen
 import com.coderabbit.demo.ui.theme.CodeRabbitDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,16 +17,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CodeRabbitDemoTheme {
-                var taskName by remember { mutableStateOf("") }
-                var selectedCategory by remember { mutableStateOf(TaskCategory.HOME) }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TaskInput(
-                        taskName = taskName,
-                        selectedCategory = selectedCategory,
-                        onNameChange = { taskName = it },
-                        onCategoryChange = { selectedCategory = it },
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TaskCreationScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
